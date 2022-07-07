910d15aeba7eca30828785cfaac0e0c8c6d1ac38
ghostcero7 <44143019+amilcaroswaldo@users.noreply.github.com>
2022-07-06 21:41:38 -0600

    Delete module-example directory

--- module-example/make-it-modular.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-const dir = process.argv[2];
-const typeFile = '.' + process.argv[3];
-const mymodule = require('./mymodule.js');
-
-mymodule(dir, typeFile, (err, files) => files.forEach(element => {console.log(element)}));
