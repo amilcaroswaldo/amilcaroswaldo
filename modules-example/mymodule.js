8a3cd17a3032400e0dc5bd3a3f6afa42c3a8eba4
ghostcero7 <44143019+amilcaroswaldo@users.noreply.github.com>
2022-07-06 21:44:01 -0600

    web
    example about modules, callbacks (async)....

---
+++ modules-example/mymodule.js
@@ -0,0 +1,14 @@
+'use strict'
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function(dir, ext, callback){
+    fs.readdir(dir, function(err, files){
+        if(err) return callback(err);
+        files = files.filter(element => '.' +ext === path.extname(element) )
+       /* files.forEach(element => {
+            ext === path.extname(element) ? console.log(element) : null;
+        });*/
+        callback(null, files);
+    })
+}
